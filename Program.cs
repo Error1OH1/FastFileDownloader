ef8da05b70a740b4f95eb0f0c6521adce61dce35
Aiden <109842140+Error1OH1@users.noreply.github.com>
2023-03-19 21:19:45 -0500

    Update Program.cs
    Updated Comments, Github decided to be silly and break on me.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using FastFileDownloader;
+using FastFileDownloader;
 using System.Net.Http;
 using System.Net.Http.Headers;
 
@@ -9,15 +9,18 @@ namespace FFastDownloader;
 
 class Program
 {
+    //Httpclient method is created outside of the methods as a public static so that it may be used as an single instance by all methods
     public static  HttpClient client = new HttpClient();
     public static async Task Main(string[] args)
     {
-        
+        //url used to retreive information
         string url = "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe";
+        //Sends the Url to and Starts the HttpClientDownloadChunking method
         HttpClientDownloadChunking(url);
-       
+
     }
 
+    //Processes the information from the link
     static void HttpClientDownloadChunking(string url)
     {
         //Creates an instance of HttpRequestMessage that uses HttpMethod.Get to retrieve information from the url
@@ -62,38 +65,54 @@ class Program
 
 
     }
+    //Processes the size & Downloads each chunk
     static void DownloadChunkAsync(string url, Stream OutPutStream, DownloadChunk chunk, HttpClient client)
     {
         //Creates a new instance of HttpRequestMessage that use the HttpMethod.Get to retrieve information from the url
         var request = new HttpRequestMessage(HttpMethod.Get, url);
-        //Gets the range(length) of each chunk and downloads
+        //Gets the range(length) of each new chunk
         request.Headers.Range = new RangeHeaderValue(chunk.Start, chunk.End);
 
+        //Sends the `request` function created ealier witht HttpCompletionOption.ResponseHeaders.Read which receives the headers as soon as they are available
         var response = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result;
+        //Once the above response is received the response.Content.ReadAsStreamAsync().Result; is use to read the contents of the response
         var stream = response.Content.ReadAsStreamAsync().Result;
+        //Once the response is read stream.CopyToAsync is used to copy the information and run it through FileStream, int.MaxValue is used to set the buffer size to the maximum value
         stream.CopyToAsync(OutPutStream, int.MaxValue).Wait();
     }
 }
 
+     //The DownloadUtils class is used to process the math required to get the size of each chunk
     class DownloadUtils
     {
+        //This method retreives the: amount of parts that the download will by chunked(partitioned) into, the total size of the file, and the extra math used to get the size of each part
         public static DownloadChunk[] GetChunks(int parts, long totalsize, long partsize)
-        {
-            DownloadChunk[] chunk = new DownloadChunk[parts];
+    {
+        //creates an instance of the method followed by the amount of partitions to be used with the math
+        DownloadChunk[] chunk = new DownloadChunk[parts];
+        //Sets the byte value for the first chunk to start at which is updated to the end value of the previous chunk
         long start = 0;
+        //Sets the end of the chunk to the partsize introduced via the math in downloadChunks the divides the overall size
         long end = partsize;
 
-            for (int i = 0; i < chunk.Length; i++)
-            {
+        //Sets the Value for i = 0 which is used to index the array, the loop continues as long as i is less than the length of the chunk array, after each loop it adds 1
+        for (int i = 0; i < chunk.Length; i++)
+        {
+            //This creates an array for each chunk to run through
             chunk[i] = new()
             {
+                //Sets the start value of each chunk to the value of `start`
                 Start = start,
-                    End = i == parts - 1 ? totalsize : end - 1
+                //Checks if i is equal to parts(partitions) - 1,if it is it assigns the value of end = totalsize, else it subtracts the end by 1
+                End = i == parts - 1 ? totalsize : end - 1
             };
 
+            //Sets the start value to the end of the previous chunk
             start = end;
+            //Increases the value of end by the size of each chunk
             end += partsize;
         }
+        //Returns the chunk aray containing the DownloadChunk object representing the chunk(partition)
         return chunk;
         }
     }

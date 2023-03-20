2b32516e49882d7842c6f615c443ba938987274a
Babb4ge <atwoodaiden1@gmail.com>
2023-03-19 21:14:52 -0500

    Commented what each line of code does.

--- Program.cs
+++ Program.cs
@@ -15,39 +15,48 @@ class Program
         
         string url = "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe";
         HttpClientDownloadChunking(url);
-        Console.WriteLine("finished");
+       
     }
 
     static void HttpClientDownloadChunking(string url)
     {
-
-        //Sends a Head request to get the the content length of the file, or the size in bytes
+        //Creates an instance of HttpRequestMessage that uses HttpMethod.Get to retrieve information from the url
         var headrequest = new HttpRequestMessage(HttpMethod.Get, url);
+        //Sends a Head request to get the the content length of the file, or the size in bytes
         var headresponse = client.SendAsync(headrequest).Result;
+        //Retrieves the ContentLength(File Size) from the headresponse
         var contentLength = headresponse.Content.Headers.ContentLength;
 
-        //calculate size of each part
+        //Sets the amount of parts
         var parts = 6;
+        //calculate size of each part
         var partsize = (long)Math.Ceiling((double)contentLength / parts);
 
-        //create an array of download chunk based on the number of parts
+        //create an array of download chunks based on the number of parts
         var downloadChunks = DownloadUtils.GetChunks(parts, contentLength.Value, partsize);
 
         //Download Each Chunk in parallel
-        using (var filestream = new FileStream(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads" + "\\steam.exe"), FileMode.Create))
+        using (var filestream = new FileStream(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads" + "\\steam.exe"), FileMode.CreateNew))
         {
-            var count = 0;
+            //Creates a count for each chunk
+            var count = 1;
+            //Creates a list of tasks to run in parallel received from the `downloadChunks` 
             var tasks = new List<Task>();
+            //Takes each chunk in downloadChunks array and adds them to the tasks list
             foreach (var chunk in downloadChunks)
             {
-                count++;
-                Console.WriteLine($"{count} chunk");
+                //For each time it downloads a full chunk it adds 1
+                Console.WriteLine($"{count++} chunk(s) completed");
+                //Each chunks from the downloadChunks is added to the List<Task> class and run through DownloadChunkAsync with the url, filestream info, chunk size, and httpclient 
                 tasks.Add(Task.Run(() => DownloadChunkAsync(url, filestream, chunk, client)));
-                
+
+
             }
-                Task.WaitAll(tasks.ToArray());
+            //Wait's for all taks in List<Task> to be downloaded and moves on
+            Task.WaitAll(tasks.ToArray());
         }
-        Console.WriteLine("download complete");
+        //Initiates after all chunks in List<Task> have finished
+        Console.WriteLine("All chunk(s) completed");
 
 
 
@@ -55,8 +64,9 @@ class Program
     }
     static void DownloadChunkAsync(string url, Stream OutPutStream, DownloadChunk chunk, HttpClient client)
     {
-        
+        //Creates a new instance of HttpRequestMessage that use the HttpMethod.Get to retrieve information from the url
         var request = new HttpRequestMessage(HttpMethod.Get, url);
+        //Gets the range(length) of each chunk and downloads
         request.Headers.Range = new RangeHeaderValue(chunk.Start, chunk.End);
 
         var response = client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead).Result;
